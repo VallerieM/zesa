caf15f3ba987dc4854b5b746a5a5763ea52d797f
Vallerie Madzikanda <ze268208@ZEDC.CO.ZW>
2019-12-02 09:26:10 +0200

    push

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:zetdc/pages/landing_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/track_application_page.dart
+++ lib/pages/track_application_page.dart
@@ -4,40 +4,35 @@ import 'package:zetdc/ui/track_application/track_application_form.dart';
 class TrackApplicationPage extends StatelessWidget {
   const TrackApplicationPage({Key key}) : super(key: key);
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          centerTitle: true ,
+          centerTitle: true,
           title: Text('TRACK APPLICATION', textAlign: TextAlign.center),
           leading: new IconButton(
-             icon: new Icon(Icons.arrow_back),
-             onPressed: () {
-               Navigator.pop(context);
-             },
+            icon: new Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
-
-         body: Padding(
-          padding: EdgeInsets.all(8.0), 
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-             Padding(
-              padding: EdgeInsets.fromLTRB(20.0, 10.0, 15.0, 10.0),
-                child: Text('Application Tracker', style: new TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ))
-            ), 
-            ApplicationForm(),
-            ]
-
-      )
-    ),
-   ),
-  );
+        body: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(20.0, 10.0, 15.0, 10.0),
+                      child: Text('Application Tracker',
+                          style: new TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold))),
+                  ApplicationForm(),
+                ])),
+      ),
+    );
   }
 }
